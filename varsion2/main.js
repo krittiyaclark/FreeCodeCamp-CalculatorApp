1853f611c1c541043990424423e1f5bf855ecdb0
Krittiya Clark <krittiyaclark@gmail.com>
2017-09-21 17:42:02 -0400

    Equal function work

--- varsion2/main.js
+++ varsion2/main.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
 
-  var add = function(a, b) {
-    a = parseInt(a);
-    b = parseInt(b);
-    return a + b;
-  }
-
-  var subtract = function(a, b) {
-    return a - b;
-  }
-
-  var multiply = function(a, b) {
-    return a * b;
-  }
-
-  var divide = function(a, b) {
-    return a / b;
-  }
-  
   var operation = "";
 
   $('.number').click(function() {
@@ -36,18 +18,9 @@ $(document).ready(function() {
      $('.screen').val('');
   });
 
-  var ops = {
-    '+': add,
-    '-': subtract,
-    'x': multiply,
-    '/': divide
-  };
-
   $('.equal').click(function() {
     var input = $('.screen').val();
-    var parts = input.match(/^(\d+)(\D)(\d+)/);
-    //var func = add + subtract + multiply + divide;
-    var result = input += parts;
-    $('.screen').val(Math.round(result));
-    });
-});
+    $('.screen').val(eval(input));
+  });
+
+ });
